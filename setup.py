1f380af43b079b43c9cadb0bf20f75ab2e970eb6
Dror Moyal <moyaldror@gmail.com>
2018-02-08 20:06:04 +0200

    Upload setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='certificate_copycat',
+  packages=['certificate_copycat'],
+  version='1.0.0',
+  description='CopyCat A Given x509 Certificate',
+  author='Dror Moyal',
+  author_email='drorm@gmail.com',
+  url='https://github.com/moyaldror/certificate_copycat',
+  download_url='https://github.com/moyaldror/certificate_copycat/archive/1.0.0.tar.gz',
+  keywords=['x509', 'certificate', 'copy'],
+  classifiers=[],
+)
