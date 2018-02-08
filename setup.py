aa4bc8aa0fb573e11f915c559f35e5edd2e90131
Dror Moyal <moyaldror@gmail.com>
2018-02-08 19:07:31 +0000

    update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='certificate_copycat',
   packages=['certificate_copycat'],
-  version='1.0.0',
+  version='1.0.1',
   description='CopyCat A Given x509 Certificate',
   author='Dror Moyal',
   author_email='drorm@gmail.com',
   url='https://github.com/moyaldror/certificate_copycat',
-  download_url='https://github.com/moyaldror/certificate_copycat/archive/1.0.0.tar.gz',
+  download_url='https://github.com/moyaldror/certificate_copycat/archive/1.0.1.tar.gz',
   keywords=['x509', 'certificate', 'copy'],
   classifiers=[],
 )
